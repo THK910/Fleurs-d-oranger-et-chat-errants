54dbbf73dc95f4cf88d02eadff5d21be90df29f7
vincent <vsneyzo@gmail.com>
2023-12-12 16:35:55 +0100

    Ajout de la section Nomination

---
+++ nomination.php
@@ -0,0 +1,7 @@
+<!-- nomination-section.php -->
+<section class="nomination fade-in-bottom">
+            <h2>Fleurs d’oranger & chats errants
+                est nominé aux Oscars Short
+                Film Animated de 2022 !</h2>
+            <img src="http://fleurs-doranger-et-chats-errants.local/wp-content/uploads/2023/11/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021_1-removebg-preview.png" alt="Nomination au Oscar">
+</section>
